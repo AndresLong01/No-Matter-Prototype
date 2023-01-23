ee31bae6c2efc8c54af0cb2d185dcafc65cae02a
Andres Long <andreslong95@gmail.com>
2023-01-22 22:22:42 -0600

    Added base sprites for sorcerer

---
+++ Assets/Scripts/Player/Sorcerer/SorcererController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SorcererController : MonoBehaviour
+{
+  private void OnEnable()
+  {
+    if (player != null)
+    {
+      player.SetClassPhysics(myBodyCollider, sorcererAnimator);
+    }
+  }
+
+  [SerializeField] Collider2D myBodyCollider;
+  [SerializeField] Animator sorcererAnimator;
+
+  [SerializeField] float attackRecoveryTime = 1f;
+
+  private PlayerController player;
+  private PlayerAbilityTracker abilityTracker;
+
+  void Start()
+  {
+    player = PlayerController.instance;
+    abilityTracker = PlayerAbilityTracker.instance;
+    player.SetClassPhysics(myBodyCollider, sorcererAnimator);
+  }
+
+  void Update()
+  {
+
+  }
+}
