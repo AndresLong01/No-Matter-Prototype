e793be2cc1206e43fd40266ed8deda2af2058dac
Andrew Lyons <aslyons001@gmail.com>
2023-03-03 19:38:48 -0500

    raycasting working, line rendering working

--- Assets/Scripts/Player/Dwight/AnimationController.cs
+++ Assets/Scripts/Player/Dwight/AnimationController.cs
@@ -4,38 +4,40 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
-    public GameObject Arrow;
-    public GameObject ArrowSpawn;
-    public CircleCollider2D ArrowCollisionCheck;
-    private Animator myAnimator;
     private LineRenderer lineRenderer;
-    private bool holdingLMB = false;
     private Vector3 releasedMousePos;
-
-    // mouse position in world space
-    private Vector2 mousePos;
-
     // for now, just the transform.position
     // or, dwight's world space position
     private Vector3 arrowSpawnPos;
-
+    private Transform ArrowSpawn;
+    public GameObject SpawnPoint;
+    private Animator myAnimator;
+    private float launchForce;
+    public GameObject Arrow;
+    // mouse position in world space
+    private Vector2 mousePos;
+    private bool holdingLMB;
     // This is the angle from a flat horizontal line
     // extending to the right from dwight rotated counter-clockwise
     // towards the mouse position
     private float angle;
 
-    private float launchForce;
-
-    // Start is called before the first frame update
     void Start()
     {
         myAnimator = gameObject.GetComponent<Animator>();
+        
         lineRenderer = gameObject.GetComponent<LineRenderer>();
         lineRenderer.positionCount = 2;
+
+        holdingLMB = false;
+
         launchForce = transform.GetComponentInParent<DwightController>().launchForce;
+
+        arrowSpawnPos = SpawnPoint.transform.position;
+
+        ArrowSpawn = SpawnPoint.transform.parent;
     }
 
-    // Update is called once per frame
     void Update()
     {
         holdingLMB = Input.GetMouseButton(0);
@@ -51,36 +53,9 @@ public class AnimationController : MonoBehaviour
         }
     }
 
-    public void ChildCollided(ArrowColliderController childCollider)
-    {
-        Debug.Log("Yo!");
-    }
-
-    private float[] GetProjectileXPoints(float[] yPoints)
-    {
-        float[] xPoints = new float[yPoints.Length];
-
-        float xStart = arrowSpawnPos.x;
-        float xEnd = mousePos.x;
-
-        float interpRate = (xEnd - xStart) / yPoints.Length;
-
-        for (int i = 0; i < yPoints.Length; i++)
-        {
-            xPoints[i] = i * interpRate;
-        }
-
-        return xPoints;
-    }
-
-    private void GetProjectilePoints()
-    {
-
-    }
-
     private void UpdatePositions()
     {
-        arrowSpawnPos = ArrowSpawn.transform.position;
+        arrowSpawnPos = SpawnPoint.transform.position;
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - arrowSpawnPos;
         angle = Mathf.Atan2(releasedMousePos.y - arrowSpawnPos.y, releasedMousePos.x - arrowSpawnPos.x) * Mathf.Rad2Deg;
     }
@@ -98,14 +73,13 @@ public class AnimationController : MonoBehaviour
             releasedMousePos = mousePos;
             myAnimator.SetTrigger("ReleasedAttack");
             myAnimator.SetBool("IsHoldingAttack", false);
-            GetProjectilePoints();
         }
     }
 
     public void Yeet()
     {
         Quaternion instantiationAngle = Quaternion.Euler(new Vector3(0, 0, angle));
-        Vector3 instantiationPosition = ArrowSpawn.transform.position;
+        Vector3 instantiationPosition = SpawnPoint.transform.position;
         GameObject ArrowIns = Instantiate(Arrow, instantiationPosition, instantiationAngle);
         ArrowIns.GetComponent<Rigidbody2D>().AddForce(releasedMousePos.normalized * launchForce, ForceMode2D.Impulse);
     }
--- Assets/Scripts/Player/Dwight/ProjectilePathDrawerController.cs
+++ Assets/Scripts/Player/Dwight/ProjectilePathDrawerController.cs
@@ -4,90 +4,127 @@ using UnityEngine;
 
 public class ProjectilePathDrawerController : MonoBehaviour
 {
-
-    private Animator myAnimator;
-    private LineRenderer lineRenderer;
-    private Transform arrowSpawnPos;
-    private Vector2 mousePos; // TODO: move this to parent context? Or leave it here? Currently duplicated on multiple scripts...
-    private float launchForce;
-    private int positionCount = 100;
     private Vector3[] currentRayPoints;
     private Vector3[] newPointsInLine;
+    private LineRenderer lineRenderer;
+    private Transform arrowSpawnPos;
+    private string projectileType;
+    private Animator myAnimator;
+    private LayerMask layerMask;
+    private bool hitRegistered; // w/ bouncy projectiles, is this a bool[]?
+    private int positionCount;
+    private float launchForce;
+    private Vector2 mousePos; // TODO: move this to parent context? Or leave it here? Currently duplicated on multiple scripts...
 
     void Start()
     {
-        lineRenderer = this.GetComponent<LineRenderer>();
         myAnimator = gameObject.GetComponentInParent<DwightController>().myAnimator;
-        arrowSpawnPos = transform.parent.Find("ArrowSpawn"); // why use transform? no gameObject?
+
         launchForce = gameObject.GetComponentInParent<DwightController>().launchForce;
+        arrowSpawnPos = transform.parent.Find("ArrowSpawn").Find("SpawnPoint"); // why use transform? no gameObject?
+
+        positionCount = 150;
+        lineRenderer = this.GetComponent<LineRenderer>();
         lineRenderer.positionCount = positionCount;
         currentRayPoints = new Vector3[positionCount];
+
+        // tilde operator inverts the mask - or, collide with all layers except ones specified here
+        layerMask = ~LayerMask.GetMask("No Collisions", "Player", "Projectiles");
+
+        projectileType = "simple";
+        hitRegistered = false;
     }
 
     void Update()
     {
         UpdatePositions();
 
-        bool hitRegistered = false;
-
         if (myAnimator.GetBool("IsHoldingAttack") && Input.GetMouseButton(0))
         {
             lineRenderer.enabled = true;
 
-            Vector2 arrowPosition = arrowSpawnPos.position;
-            Vector2 arrowVelocity = mousePos.normalized * launchForce;
-
-            for (int i=0; i < positionCount-1; i++)
+            if (projectileType == "simple")
             {
-                lineRenderer.SetPosition(i, new Vector3(arrowPosition.x, arrowPosition.y, 0));
-                // add gravity vector * timedelta to velocity vector
-                // or, add a frame of gravity influence to arrow velocity vector
-                arrowVelocity += Physics2D.gravity * Time.fixedDeltaTime;
-
-                // add velocity vector * timedelta to current position
-                // or, add a frame of calculated velocity influence to arrow position vector
-                arrowPosition += arrowVelocity * Time.fixedDeltaTime;
-
-                // store this
-                currentRayPoints[i] = arrowPosition;
-
-                // skip this if we don't have at least two points yet
-                if (i >= 1)
-                {
-                    // null if we don't collide with anything between (start, end)
-                    if (Physics2D.Linecast(currentRayPoints[i-1], currentRayPoints[i]))
-                    {
-                        newPointsInLine = new Vector3[i+1];
-
-                        for (int j=0; j<newPointsInLine.Length; j++)
-                        {
-                            // transfer the points up to this point to the new points
-                            newPointsInLine[j] = currentRayPoints[j];
-                        }
-
-                        hitRegistered = true;
-
-                        break;
-                    }
-                }
-            }
-
-            if (hitRegistered)
-            {
-                lineRenderer.positionCount = newPointsInLine.Length;
-                lineRenderer.SetPositions(newPointsInLine);
-            }
-            else
-            {
-                // something is going wrong here, I think. We're getting a point sometimes that 
-                // lands us back at the origin point. Also we get index out of range errors. Not sure why >:(
-                lineRenderer.positionCount = currentRayPoints.Length;
-                lineRenderer.SetPositions(currentRayPoints);
+                DrawSimpleProjectilePath();
             }
         }
         else
         {
             lineRenderer.enabled = false;
+        }
+    }
+
+    private void DrawSimpleProjectilePath()
+    {
+        // initial launch vector + position vector
+        Vector2 arrowPosition = arrowSpawnPos.position;
+        Vector2 arrowVelocity = mousePos.normalized * launchForce;
+
+        // run through the max amount every time
+        // TODO: See how we could increase the distance between the calculated points
+        // which would allow us to reduce the # of points. Might be necessary if this is
+        // computationally expensive?
+        for (int i = 0; i < positionCount; i++)
+        {
+            if (i == 0)
+            {
+                currentRayPoints[i] = arrowPosition;
+                continue;
+            }
+
+            // add gravity vector * timedelta to velocity vector
+            // or, add a frame of gravity influence to arrow velocity vector
+            arrowVelocity += Physics2D.gravity * Time.fixedDeltaTime;
+
+            // add velocity vector * timedelta to current position
+            // or, add a frame of calculated velocity influence to arrow position vector
+            arrowPosition += arrowVelocity * Time.fixedDeltaTime;
+
+            currentRayPoints[i] = arrowPosition;
+
+            if (i >= 1)
+            {
+                RaycastHit2D hit = Physics2D.Linecast(currentRayPoints[i - 1], currentRayPoints[i], layerMask);
+                if (hit)
+                {
+                    // if i === 50, then point 50 -> 51 had the collision (0-indexed)
+                    // so make our new points 51 units in length
+                    newPointsInLine = new Vector3[i + 1];
+
+                    // only grab the i-1 points, because the i-th point had the collision,
+                    // and is probably inside the collider
+                    for (int j = 0; j < newPointsInLine.Length - 1; j++)
+                    {
+                        newPointsInLine[j] = currentRayPoints[j];
+                    }
+
+                    // set the last point to the raycast collision point, which lets us
+                    // draw the line perfectly up to the collider
+                    newPointsInLine[i] = hit.point;
+                    hitRegistered = true;
+                    break;
+                }
+            }
+        }
+
+        if (hitRegistered)
+        {
+            // we registered a hit, set the line points to equal the valid,
+            // non-colliding points we just calculated.
+            lineRenderer.positionCount = newPointsInLine.Length;
+            lineRenderer.SetPositions(newPointsInLine);
+        }
+        else
+        {
+            // something is going wrong here, I think. We're getting a point sometimes that 
+            // lands us back at the origin point. Also we get index out of range errors. Not sure why >:(
+            lineRenderer.positionCount = currentRayPoints.Length;
+            lineRenderer.SetPositions(currentRayPoints);
+
+            // was getting an issue where if we log 90/100 points one time, then log 40 points another,
+            // there are 50 lingering points that draw the line all over the place. Not sure if this is jank or not,
+            // but resetting the point array seems to work alright.
+            currentRayPoints = new Vector3[positionCount];
         }
     }
 
